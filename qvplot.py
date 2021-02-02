9dec62a3c2d19e4f3272b3ddf2111a73c0fe5cd2
kyle <kylekroll@outlook.com>
2021-02-02 17:59:31 -0500

    Changed datasets. Added more info on hover

--- qvplot.py
+++ qvplot.py
@@ -2,49 +2,87 @@
     BIOT670 Capstone Project - Quad Viewer
     plotting functions
 '''
+import json
 import math
 
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_table
 import pandas as pd
 import plotly.graph_objects as go
-
+from dash.dependencies import Input, Output
 
 app = dash.Dash(__name__)
-df = pd.read_csv('het_coro_aorta.tsv', sep='\t')
-df['Mean HET_AORTA'] = df['Mean HET_AORTA'].apply(lambda x: math.log2(x+1))
-df['Mean HET_CORO'] = df['Mean HET_CORO'].apply(lambda x: math.log2(x+1))
-df['NEG Mean HET_AORTA'] = df['Mean HET_AORTA'].apply(lambda x: x*-1)
-df['NEG Mean HET_CORO'] = df['Mean HET_CORO'].apply(lambda x: x*-1)
+styles = {
+    'pre': {
+        'border': 'thin lightgrey solid',
+        'overflowX': 'scroll'
+    }
+}
+xpos = 'DB_CORO_MEAN'
+ypos = 'HET_CORO_MEAN'
+xneg = 'KOHET_CORO_MEAN'
+yneg = 'KOKO_CORO_MEAN'
+df = pd.read_csv('coro_data.tsv', sep='\t')
+df[xpos] = df[xpos].apply(lambda x: x)
+df[ypos] = df[ypos].apply(lambda x: x)
+
 fig = go.Figure()
-fig.add_scatter(x=df['Mean HET_AORTA'],
-                y=df['Mean HET_CORO'],
+fig.add_scatter(x=df[xpos],
+                y=df[ypos],
                 mode='markers',
-                fillcolor='blue',
+                marker_color='blue',
                 text=df['Accession_Number'])
-fig.add_scatter(x=df['NEG Mean HET_AORTA'],
-                y=df['Mean HET_CORO'],
+fig.add_scatter(x=df[xpos].apply(lambda x: x * -1),
+                y=df[ypos],
                 mode='markers',
-                fillcolor='blue',
+                marker_color='blue',
                 text=df['Accession_Number'])
-fig.add_scatter(x=df['NEG Mean HET_AORTA'],
-                y=df['NEG Mean HET_CORO'],
+fig.add_scatter(x=df[xpos].apply(lambda x: x * -1),
+                y=df[ypos].apply(lambda x: x * -1),
                 mode='markers',
-                fillcolor='blue',
+                marker_color='blue',
                 text=df['Accession_Number'])
-fig.add_scatter(x=df['Mean HET_AORTA'],
-                y=df['NEG Mean HET_CORO'],
+fig.add_scatter(x=df[xpos],
+                y=df[ypos].apply(lambda x: x * -1),
                 mode='markers',
-                fillcolor='blue',
+                marker_color='blue',
                 text=df['Accession_Number'])
-fig.update_layout(height=700)
+fig.update_layout(height=500, showlegend=False)
 app.layout = html.Div([
     dcc.Graph(
-        id='graph',
-        figure=fig
-    )
+        id='basic-interactions',
+        figure=fig,
+        clear_on_unhover=True
+    ),
+
+    html.Div(className='row', children=[
+        html.Div([
+            dcc.Markdown("""
+                **Hover Data**
+
+                Mouse over values in the graph.
+            """),
+            html.Pre(id='hover-data', style=styles['pre'])
+        ], className='three columns')
+    ])
 ])
+
+
+@app.callback(
+    Output('hover-data', 'children'),
+    Input('basic-interactions', 'hoverData'))
+def display_hover_data(hoverData):
+    name = hoverData['points'][0]['text']
+    path = df.loc[df['Accession_Number'] == name, 'All_Pathways'].values[0]
+    xpos_val = df.loc[df['Accession_Number'] == name, xpos].values[0]
+    ypos_val = df.loc[df['Accession_Number'] == name, ypos].values[0]
+    xneg_val = df.loc[df['Accession_Number'] == name, xneg].values[0]
+    yneg_val = df.loc[df['Accession_Number'] == name, yneg].values[0]
+    blah = f'Protein:\t{name}\nPathways:\t{path}\n{xpos}:\t{xpos_val}\n{ypos}:\t{ypos_val}\n{xneg}:\t{xneg_val}\n{yneg}:\t{yneg_val}'
+    return blah
+
 
 if __name__ == '__main__':
     app.run_server(debug=False)
